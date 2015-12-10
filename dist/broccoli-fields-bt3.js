5c77812a0a4d1034866f1a148f0b049096d01109
Misaki Shibata <misaki.pink@gmail.com>
2015-12-10 21:10:24 +0900

    名前をBroccoliModuleBootstrap3に変更

--- dist/broccoli-fields-bt3.js
+++ dist/broccoli-fields-bt3.js
@@ -17152,7 +17152,7 @@ module.exports = phpjs;
 
 },{}],31:[function(require,module,exports){
 (function(window){
-	window.broccoliBootstrap3 = {
+	window.BroccoliModuleBootstrap3 = {
 		"AlertField": require('../libs/bootstrap3-alert-client.js'),
 		"BadgeField": require('../libs/bootstrap3-badge-client.js'),
 		"ButtonField": require('../libs/bootstrap3-button-client.js'),

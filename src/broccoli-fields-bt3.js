593b435987151950fac9eb7010cf204827a80993
Misaki Shibata <misaki.pink@gmail.com>
2015-12-10 20:31:08 +0900

    クライアントjsの構造オブジェクト型に変更

---
+++ src/broccoli-fields-bt3.js
@@ -0,0 +1,9 @@
+(function(window){
+	window.broccoliBootstrap3 = {
+		"AlertField": require('../libs/bootstrap3-alert-client.js'),
+		"BadgeField": require('../libs/bootstrap3-badge-client.js'),
+		"ButtonField": require('../libs/bootstrap3-button-client.js'),
+		"GlyphiconsField": require('../libs/bootstrap3-glyphicons-client.js'),
+		"LabelsField": require('../libs/bootstrap3-labels-client.js')
+	};
+})(window);
